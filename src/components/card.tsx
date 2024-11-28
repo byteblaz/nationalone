d2e65e99031ab60ad33a3d74e274d73588493dfd
byteblaz <sreeramskumar8@gmail.com>
2024-11-28 18:54:15 +0530

    a

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,7 +6,7 @@ export const Card = ({className,children,}: PropsWithChildren<{className?:string
 
 }>) => {
     return (
-        <div className={twMerge("backdrop-blur-[4px] backdrop-saturate-[100%] bg-[#ffffff] bg-opacity-10 border border-opacity-20 border-[#ffffff] shadow-[0px_10px_39px_10px_rgba(164,120,165,0.3)_inset] bg-[#ffffff] rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none sm:-mx-2", className)}>
+        <div className={twMerge("backdrop-blur-[4px] backdrop-saturate-[100%] bg-[#ffffff] bg-opacity-10 border border-opacity-20 border-[#ffffff]  rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none sm:-mx-2", className)}>
               <div className="absolute inset-0 -z-10 opacity-5" style={{
                 backgroundImage: `url(${grainImage.src})`,
               }}
