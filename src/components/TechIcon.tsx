32e9215ff6807b58f12a48a1edf89b4d74d41df4
byteblaz <sreeramskumar8@gmail.com>
2024-11-29 00:57:17 +0530

    img

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -6,8 +6,8 @@ export const TechIcon = ({component}:{component:React.ElementType}) => {
     <Component className="size-6 fill-[url(#tech-icon-gradient)]" />
     <svg className="size-0 absolute">
     <linearGradient id="tech-icon-gradient">
-        <stop offset="0%" stop-color="rgb(22, 163, 74)" /> 
-          <stop offset="100%" stop-color="rgb(22, 163, 74)" /> 
+        <stop offset="0%" stopColor="rgb(22, 163, 74)" /> 
+          <stop offset="100%" stopColor="rgb(22, 163, 74)" /> 
     </linearGradient>
 
     </svg>
